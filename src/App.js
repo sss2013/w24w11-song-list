6ebe1de60b2cc2caeb2c5591d17b9e85f5376985
sss2013 <kshi9478@gmail.com>
2024-11-27 15:26:15 +0900

    노래 배열

--- src/App.js
+++ src/App.js
@@ -1,37 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import Container from './Container.js'
 
-function App() {
+const App = () => {
+  const songs = [
+    'Dear..',
+    'Diver',
+    'Soul love',
+  ]
+
   return (
-    <div className="App">
-      <Header/>
-      <Playlist/>
-      <Container/>
-    </div>
-  );
-}
-
-function Playlist() {
-  return (
-    <div className="playlist">프로그래밍하면서 듣고 싶은 노래</div>
-  )
-}
-
-function Header(){
-  return (
-    <h1>React 프론트엔드 프로그래밍</h1>
-  )
-}
-
-function Container(){
-  return(
-    <div className="container">
-      <a href="https://www.youtube.com/results?search_query=Dear..">
-        <img src="https://picsum.photos/600/150?random=1" alt="랜덤 이미지1"></img>
-        <div className="song-title">Dear..</div>
-      </a>
+    <div>
+      <Header />
+      <Playlist 
+        title = "프로그래밍하면서 듣고 싶은 노래"
+        listSong={songs}/>
     </div>
   )
 }
+
+function Header() {
+  return (
+    <div className="playlist">React 프로그래밍</div>
+  )
+}
+
+const Playlist = props => {
+  return (
+    <div className='playlist'>
+      <div className='playlist'>{props.title}</div>
+      {
+        props.listSong.map((song,index)=> (
+          <Container key={index} title={song} index={index}/>
+        ))
+      }
+    </div>
+  )
+}
+
 
 export default App;
